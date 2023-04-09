ccf03b93b2724337eb24979aa5065c6fd9d182ad
Vladislav Pishchikov <vladislavs@scandiweb.com>
2023-04-09 09:53:17 +0300

    Updated dev.properties.dist and ApiPrefixHelper

--- src/main/java/handson/impl/ApiPrefixHelper.java
+++ src/main/java/handson/impl/ApiPrefixHelper.java
@@ -4,7 +4,7 @@ public enum ApiPrefixHelper {
     API_DEV_CLIENT_PREFIX("dev-admin."),
     API_PROJECT_SYNC_CLIENT_PREFIX("dev-project-sync-admin."),
     API_DEV_IMPORT_PREFIX("dev-import-admin."),
-    API_STORE_CLIENT_PREFIX("dev-berlin-store-admin."),
+    API_STORE_CLIENT_PREFIX("dev-europe-store-admin."),
     API_ME_CLIENT_PREFIX("dev-me-client."),
     API_STORE_ME_CLIENT_PREFIX("dev-store-me-client.");
 
